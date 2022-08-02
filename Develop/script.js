bd59dfc3549ee7d9678c6a1b4d374344f0249585
kalina <kalina.boksa@gmail.com>
2022-08-02 18:02:36 -0500

    adding alert

--- Develop/script.js
+++ Develop/script.js
@@ -69,6 +69,9 @@ function generatePassword() {
     finalPassword[i] = guaranteedCharacters[i];
     console.log(typeof finalPassword, guaranteedCharacters[i]);
   }
+  // if (numberCharacters <8 || >128){
+  //   alert("It is recommended to have a password between 8 and 128 characters long.")
+  // }
   return finalPassword.join("");
 }
 
